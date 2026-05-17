3a456c82e5e0192d5eceb6f19c60f9b93399edfa
Mixalhs <mixalhsd@outlook.com.gr>
2026-05-17 15:16:39 +0300

    Modified adminGlobalAppoinments

--- src/main/java/com/adminUtils/adminGlobalAppoinments.java
+++ src/main/java/com/adminUtils/adminGlobalAppoinments.java
@@ -95,7 +95,6 @@ public class adminGlobalAppoinments extends javax.swing.JFrame {
         jLabel1.setToolTipText("");
 
         usernameShow.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
-        usernameShow.setForeground(java.awt.Color.white);
         jScrollPane1.setViewportView(usernameShow);
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
